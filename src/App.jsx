03fe373a29c2f13eee0e39d02429ee2b7019c851
Samuel Obi <samobi588@gmail.com>
2024-09-20 14:59:26 +0100

    removed the Pointer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Main from "./Components/Main"
 import ProfilePicture from "./Components/ProfilePicture"
 import Socials from "./Components/Socials"
-import Pointer from "./Components/Pointer"
 
 export default function App() {
   return (
     <div className="app-container">
-        <Pointer />
        <ProfilePicture />
        <Main />
        <Socials />
